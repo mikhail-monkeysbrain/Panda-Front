861ee265dfc02f05aafac7b2516112abb8cb142a
S. Orlov <grcss@rambler.ru>
2017-12-04 16:51:34 +0300

    base pagepiling

--- app/src/js/main/main.js
+++ app/src/js/main/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
   $('#pagepiling').pagepiling();
+  $('#pagepiling .pageDown').click(function () {
+    $.fn.pagepiling.moveSectionDown();
+  });
+  $('#pagepiling .pageUp').click(function () {
+    $.fn.pagepiling.moveTo(1);
+  });
 });
