8a2fea4f3d032d29b81f197e55dd070fb6bca5de
S. Orlov <grcss@rambler.ru>
2017-12-06 18:05:40 +0300

    alpha home desktop

--- app/src/js/main/main.js
+++ app/src/js/main/main.js
@@ -18,7 +18,8 @@ $(document).ready(function () {
     mouseDrag:0,
     touchDrag:0,
     loop:1,
-  }).on('changed.owl.carousel', sofaChange);
+  });
+  // .on('changed.owl.carousel', sofaChange);
 
   function sofaChange() {
     var sofaType = $('.sofa-carousel .img-area .owl-item.active').next().find('.sofa-carousel__type').text();
@@ -29,8 +30,54 @@ $(document).ready(function () {
   }
 
   $('.sofa-carousel .sofa-carousel__next').click(function () {
+    sofaChange();
     $('.sofa-carousel .img-area').trigger('next.owl.carousel');
   });
+
+  $('.sertificates-carousel').owlCarousel({
+    items:4,
+    stagePadding:75,
+    margin:0,
+    autoplay:0,
+    mouseDrag:0,
+    touchDrag:0,
+    loop:1,
+    slideBy:4,
+  })
+
+  $('.home-sertificates .sofa-carousel__next').click(function () {
+    $('.sertificates-carousel').trigger('next.owl.carousel');
+  });
+
+  $('.home-sertificates .sofa-carousel__next').on('mouseover', function () {
+    $('.sertificates-carousel .owl-stage').css('left', '-25%');
+    $('.home-sertificates .sofa-carousel__next').on('mouseleave', function () {
+      $('.sertificates-carousel .owl-stage').css('left', '0');
+    });
+  });
+
+  $('.news-carousel').owlCarousel({
+    items:4,
+    stagePadding:75,
+    margin:0,
+    autoplay:0,
+    mouseDrag:0,
+    touchDrag:0,
+    loop:1,
+    slideBy:4,
+  })
+
+  $('.home-news .sofa-carousel__next').click(function () {
+    $('.news-carousel').trigger('next.owl.carousel');
+  });
+
+  $('.home-news .sofa-carousel__next').on('mouseover', function () {
+    $('.news-carousel .owl-stage').css('left', '-25%');
+    $('.home-news .sofa-carousel__next').on('mouseleave', function () {
+      $('.news-carousel .owl-stage').css('left', '0');
+    });
+  });
   // Carousel end
+
 
 });
